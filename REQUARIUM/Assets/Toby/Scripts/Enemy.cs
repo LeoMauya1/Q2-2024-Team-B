34e8d2314735d4cf5e67908d4edfc41890a56992
MUKILTEO\1717222 <tobiahsday@gmail.com>
2024-11-21 13:27:30 -0800

    Continuing on Enemy AI

---
+++ REQUARIUM/Assets/Toby/Scripts/Enemy.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using UnityEngine;
+
+public partial class Enemy : MonoBehaviour
+{
+}
---
+++ REQUARIUM/Assets/Toby/Scripts/EnemyType.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public partial class Enemy
+{
+    [System.Serializable]
+    public struct EnemyType
+    {
+        public string name;
+        public float speed;
+        public OnCollisionEnter collision;
+        public List<Node> nodes;
+        public Node currentNode;
+        public Node nextNode;
+        public Animator animator;
+    }
+}
