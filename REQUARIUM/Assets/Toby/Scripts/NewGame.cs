98d7e6534b287f27cd70220da5a164807d3b16eb
MUKILTEO\1717854 <1717854@mukilteo.wednet.edu>
2025-01-14 13:26:14 -0800

    loading screen and main menu screen in progress

--- REQUARIUM/Assets/Toby/Scripts/NewGame.cs
+++ REQUARIUM/Assets/Toby/Scripts/NewGame.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NewGame : MonoBehaviour
 {
     public UnityEvent onNewGame;
     public UnityEvent onContinue;
-
+    public Image LoadingBarFill;
+    public GameObject loadingScreen;
+    private float progressBar;
     public void OnEnable()
     {
         if (SaveDataManager.Instance.daveSata.isNewGame)
@@ -21,10 +24,33 @@ public class NewGame : MonoBehaviour
         }
     }
 
-    public void LoadNewGame()
+    public void LoadNewGame(int sceneID)
     {
         SaveDataManager.Instance.daveSata.isNewGame = true;
         SaveDataManager.Instance.daveSata = SaveDataManager.Instance.defaultData;
-        SceneManager.LoadScene("Main Level");
+        StartCoroutine(LoadSceneAsync(sceneID));
+        
+        
+        
+        SceneManager.LoadScene(sceneID);
     }
+
+    IEnumerator LoadSceneAsync(int sceneID)
+   {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+
+
+        loadingScreen.SetActive(true);
+
+
+        while (!operation.isDone)
+       {
+
+            progressBar = Mathf.Clamp01(operation.progress/ 0.9f);
+
+           LoadingBarFill.fillAmount = progressBar;
+            
+           yield return null;
+        }
+   }
 }
